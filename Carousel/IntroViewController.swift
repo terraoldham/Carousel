27c7b079151b32f82a04827e0355cd0658693574
Terra Oldham <toldham@hearsaycorp.com>
2014-10-15 22:41:39 -0700

    Initial Commit
    Initial and incomplete commit

---
+++ Carousel/IntroViewController.swift
@@ -0,0 +1,39 @@
+//
+//  IntroViewController.swift
+//  Carousel
+//
+//  Created by Terra Oldham on 10/14/14.
+//  Copyright (c) 2014 HearsaySocial. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 1168)
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Carousel/SignInViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SignInViewController.swift
+//  Carousel
+//
+//  Created by Terra Oldham on 10/14/14.
+//  Copyright (c) 2014 HearsaySocial. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func onSignIn(sender: AnyObject) {
+        if emailField.text.isEmpty && passwordField.text.isEmpty {
+            var alertView = UIAlertView(title: "Sign In Error", message: "Please enter the correct credentials.", delegate: self, cancelButtonTitle: "OK")
+            alertView.show()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
